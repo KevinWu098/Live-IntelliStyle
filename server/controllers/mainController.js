e0cb679dbd5b2c7cd31fdd1d49eb4db650251b18
Kevin Wu <kevinwu098@gmail.com>
2023-08-12 17:13:21 -0700

    Initial Commit

---
+++ server/controllers/mainController.js
@@ -0,0 +1,22 @@
+// GET Homepage
+exports.homepage = async (req, res) => {
+    const locals = {
+        title: "NodeJS Notes",
+        description: 'Free NodeJS Notes App',
+    }
+
+    res.render('index', {
+        locals,
+        layout: '../views/layouts/front-page'
+    })
+}
+
+// GET About
+exports.about = async (req, res) => {
+    const locals = {
+        title: "About - NodeJS Notes",
+        description: 'Free NodeJS Notes App',
+    }
+
+    res.render('about', locals)
+}
