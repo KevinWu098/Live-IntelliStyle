ba610a6ef13b93414c3053e3002a642f83fb3dde
Kevin Wu <kevinwu098@gmail.com>
2023-08-12 18:49:47 -0700

    Added Google Auth

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,12 +1,43 @@
+const Note = require("../models/Notes");
+const mongoose = require('mongoose')
+
 // GET Dashboard
 exports.dashboard = async (req, res) => {
+    let perPage = 12;
+    let page = req.query.page || 1;
+  
     const locals = {
-        title: "Dashboard",
-        description: 'Free NodeJS Notes App',
-    }
-
-    res.render('dashboard/index', {
+      title: "Dashboard",
+      description: "Free NodeJS Notes App.",
+    };
+  
+    try {
+      // Mongoose "^7.0.0 Update
+    //   const notes = await Note.aggregate([
+    //     { $sort: { updatedAt: -1 } },
+    //     { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+    //     {
+    //       $project: {
+    //         title: { $substr: ["$title", 0, 30] },
+    //         body: { $substr: ["$body", 0, 100] },
+    //       },
+    //     },
+    //   ])
+    //   .skip(perPage * page - perPage)
+    //   .limit(perPage)
+    //   .exec();
+  
+    //   const count = await Note.count();
+  
+      res.render('dashboard/index', {
+        userName: req.user.firstName,
         locals,
-        layout: '../views/layouts/dashboard'
-    })
-}
+        // notes,
+        layout: "../views/layouts/dashboard",
+        current: page,
+        // pages: Math.ceil(count / perPage)
+      });
+     } catch (error) {
+      console.log(error);
+    }
+  };
