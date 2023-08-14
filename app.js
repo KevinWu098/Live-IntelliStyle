f09d0912d34eed0f59fc93ec5450da526650a095
Kevin Wu <kevinwu098@gmail.com>
2023-08-14 10:37:43 -0700

    Minor cleanup to session age

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
         store: MongoStore.create({
             mongoUrl: process.env.MONGODB_URL,
         }),
-        cookie: { maxAge: new Date(Date.now() + 36000000) },
+        cookie: { maxAge: 36000000 },
     })
 );
 
